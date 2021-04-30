662bf950c69c43d4001ef430903d73a487f3f111
Kaushik Sivashankar <kaushik.sivashankar@gmail.com>
2021-04-30 10:59:44 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Rnn-game-bot
+A tictactoe playing bot trained on LSTM.
+
+# Usage
+1. Generate dataset using tictactoe.py, which is an implementation of Monte Carlo Tree Search.
+2. Use this dataset to train LSTM in LSTM.ipynb (or use saved tflite model).
---
+++ src/tictactoe.py
@@ -0,0 +1,135 @@
+"""
+An example implementation of the abstract Node class for use in MCTS
+
+If you run this file then you can play against the computer.
+
+A tic-tac-toe board is represented as a tuple of 9 values, each either None,
+True, or False, respectively meaning 'empty', 'X', and 'O'.
+
+The board is indexed by row:
+0 1 2
+3 4 5
+6 7 8
+
+For example, this game board
+O - X
+O X -
+X - -
+corrresponds to this tuple:
+(False, None, True, False, True, None, True, None, None)
+"""
+
+from collections import namedtuple
+from random import choice
+from mcts import MCTS, Node
+import json
+
+_TTTB = namedtuple("TicTacToeBoard", "tup turn winner terminal rand")
+
+# Inheriting from a namedtuple is convenient because it makes the class
+# immutable and predefines __init__, __repr__, __hash__, __eq__, and others
+class TicTacToeBoard(_TTTB, Node):
+    def find_children(board):
+        if board.terminal:  # If the game is finished then no moves can be made
+            return set()
+        # Otherwise, you can make a move in each of the empty spots
+        return {
+            board.make_move(i) for i, value in enumerate(board.tup) if value is None
+        }
+
+    def find_random_child(board):
+        if board.terminal:
+            return None  # If the game is finished then no moves can be made
+        empty_spots = [i for i, value in enumerate(board.tup) if value is None]
+        return board.make_move(choice(empty_spots),rand=True)
+
+    def reward(board):
+        if not board.terminal:
+            raise RuntimeError(f"reward called on nonterminal board {board}")
+        if board.winner is board.turn:
+            # It's your turn and you've already won. Should be impossible.
+            raise RuntimeError(f"reward called on unreachable board {board}")
+        if board.turn is (not board.winner):
+            return -1  # Your opponent has just won. Bad.
+        if board.winner is None:
+            return 0  # Board is a tie
+        # The winner is neither True, False, nor None
+        raise RuntimeError(f"board has unknown winner type {board.winner}")
+
+    def is_terminal(board):
+        return board.terminal
+
+    def make_move(board, index, rand=False):
+        tup = board.tup[:index] + (board.turn,) + board.tup[index + 1 :]
+        turn = not board.turn
+        winner = _find_winner(tup)
+        is_terminal = (winner is not None) or not any(v is None for v in tup)
+        return TicTacToeBoard(tup, turn, winner, is_terminal, rand)
+
+    def to_pretty_string(board):
+        to_char = lambda v: ("X" if v is True else ("O" if v is False else " "))
+        rows = [
+            [to_char(board.tup[3 * row + col]) for col in range(3)] for row in range(3)
+        ]
+        return (
+            "\n  1 2 3\n"
+            + "\n".join(str(i + 1) + " " + " ".join(row) for i, row in enumerate(rows))
+            + "\n"
+        )
+
+
+def play_game():
+    global data
+    tree = MCTS()
+    board = new_tic_tac_toe_board()
+    while True:
+        for _ in range(100):
+            tree.do_rollout(board)
+        new_board = tree.choose(board)
+        if not new_board.rand:
+            state = list(map(lambda x: '0' if x is None else '1' if x else '-1', 
+                    board.tup))
+            data['State'].append(state)
+            data['Turn'].append('1' if board.turn==True else '-1' if board.turn==False else '0')
+            for i in range(9):
+                if board.tup[i]!=new_board.tup[i]:
+                    data['Action'].append(i)
+        board = new_board
+        print(board.to_pretty_string())
+        if board.winner is not None:
+            print('X wins' if board.winner else 'O wins')
+            print()
+        if board.terminal:
+            break
+
+
+def _winning_combos():
+    for start in range(0, 9, 3):  # three in a row
+        yield (start, start + 1, start + 2)
+    for start in range(3):  # three in a column
+        yield (start, start + 3, start + 6)
+    yield (0, 4, 8)  # down-right diagonal
+    yield (2, 4, 6)  # down-left diagonal
+
+
+def _find_winner(tup):
+    "Returns None if no winner, True if X wins, False if O wins"
+    for i1, i2, i3 in _winning_combos():
+        v1, v2, v3 = tup[i1], tup[i2], tup[i3]
+        if False is v1 is v2 is v3:
+            return False
+        if True is v1 is v2 is v3:
+            return True
+    return None
+
+
+def new_tic_tac_toe_board():
+    return TicTacToeBoard(tup=(None,) * 9, turn=True, winner=None, terminal=False, rand=False)
+
+
+if __name__ == "__main__":
+    data = {'State':[], 'Turn':[], 'Action':[]}
+    for _ in range(300):
+        play_game()
+    with open('dataset.json','w+') as handle:
+        json.dump(data, handle, indent=2)
